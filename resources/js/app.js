d12927907488c560dc59a898a629b118700b6f3f
dave <jezreldavesondia@gmail.com>
2020-01-16 09:26:52 +0800

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import router from './router.js'
+
+
+import Index from './components/Index.vue'
+import ExampleComponent from './components/ExampleComponent.vue'
+
+Vue.use(Vuetify)
+
+
+
+
+const app = new Vue({
+    el: '#app',
+    template: '<Index></Index>',
+    components: {Index},
+    router,
+    vuetify: new Vuetify(),
+})
